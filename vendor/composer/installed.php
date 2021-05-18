2a2e2d59a78984a1de84ecd8202fd6dcedfc7313
kokeshi-2013 <79235524+kokeshi-2013@users.noreply.github.com>
2021-05-18 12:36:34 +0900

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1e4e168b57ffbd312fad6c701997844db1272151',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1e4e168b57ffbd312fad6c701997844db1272151',
+    ),
+  ),
+);
